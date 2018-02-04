f7fc6f7525c1703a853b69dd3c990cfcc8e55dec
Palash Roy <cstpalash@gmail.com>
2018-02-04 11:45:29 +0530

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Please wait while we are authenticating you to SCRIBE</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
